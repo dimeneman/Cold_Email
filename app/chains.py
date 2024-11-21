61cdd7e99c47f6909eaee2d579d5de9ed614bb79
dimeneman <146258763+dimeneman@users.noreply.github.com>
2024-11-21 23:43:19 +0530

    initial commit

---
+++ app/chains.py
@@ -0,0 +1,65 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`,'company's name', `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+Subject: Application for [Job Position] at [company's name]
+
+Hi [companie's name],
+
+I hope this email finds you well. My name is [Eman Gope], a [Computer Science] student at [SIT]. 
+I am excited to apply for the [Job Position] at [company's name].
+With a background in [relevant skills or experiences], I look forward to contributing to [company's name]’s innovative projects.
+During my time at my graduation, I’ve gained experience through projects . 
+I am eager to bring my [mention any unique skills or strengths] to your team.
+I have attached my resume for your review. Thank you for considering my application. 
+I look forward to discussing how my background aligns with [company's name]’s goals.
+Best regards,
+[Eman Gope]
+[gopeeman87@gmail.com]
+[+91 7501217770]
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
